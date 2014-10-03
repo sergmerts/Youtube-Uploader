1cf925b9329fc0fb814846ca90b88a5fbe5fcca9
Sergii Mertsalov <sergmerts@gmail.com>
2014-10-03 13:41:34 -0700

    add validation for answer on video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -19,8 +19,8 @@ class VideosController < ApplicationController
     yt_video = youtube.video_by(video_uid)
     v.title = yt_video.title
     v.description = yt_video.description
-    answer = params[:answer]
     v.save
+    answer = params[:answer]
     flash[:success] = 'Thanks for sharing your video!'
     redirect_to root_url
   end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,3 @@
 class Video < ActiveRecord::Base
+	validate :answer, presence: true
 end
